be7d4225aa6a298eca090e96edb8b7eaa6847963
Arif Rachman <emailearif@gmail.com>
2025-11-06 09:13:07 +0700

    menambahkan tool tips

--- src/app/components/monitoring-ro/monitoring-ro.component.ts
+++ src/app/components/monitoring-ro/monitoring-ro.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-monitoring-ro',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './monitoring-ro.component.html',
   styleUrl: './monitoring-ro.component.scss'
 })
 export class MonitoringRoComponent {
+  showTooltip = false;
+  tooltipContent = '';
+  tooltipPosition = { x: 0, y: 0 };
 
+  onMouseEnter(event: MouseEvent, detail: string): void {
+    this.tooltipContent = detail;
+    this.showTooltip = true;
+    this.updateTooltipPosition(event);
+  }
+
+  onMouseLeave(): void {
+    this.showTooltip = false;
+    this.tooltipContent = '';
+  }
+
+  onMouseMove(event: MouseEvent): void {
+    if (this.showTooltip) {
+      this.updateTooltipPosition(event);
+    }
+  }
+
+  private updateTooltipPosition(event: MouseEvent): void {
+    this.tooltipPosition = {
+      x: event.clientX + 10,
+      y: event.clientY - 10
+    };
+  }
 }
